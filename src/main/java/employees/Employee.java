7a99aab7d45b722cd1dde111eece7c991ea7ae3b
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-01-22 11:18:46 -0300

    Initial Commit

--- src/main/java/employees/Employee.java
+++ src/main/java/employees/Employee.java
@@ -1,4 +1,50 @@
 package employees;
 
 public class Employee {
+
+    private Integer id;
+    private String name;
+    private Double salary;
+
+    public Employee() {
+
+    }
+
+    public Employee(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return this.getId() + ", " + this.getName() + ", " + String.format("%.2f", this.getSalary());
+    }
+
+    public void increaseSalary(double percentage) {
+        this.salary += this.salary * (percentage / 100.00);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }
--- src/main/java/employees/Program.java
+++ src/main/java/employees/Program.java
@@ -1,4 +1,62 @@
 package employees;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many employees will be registered? ");
+        int n = input.nextInt();
+        input.nextLine();
+
+        List<Employee> employees = new ArrayList<>();
+
+        for (int i=0; i<n; i++) {
+            System.out.println("Employee #" + (i+1) + ":");
+            System.out.print("Id: ");
+            Integer id = input.nextInt();
+            input.nextLine();
+            System.out.print("Name: ");
+            String name = input.nextLine();
+            System.out.print("Salary: ");
+            Double salary = input.nextDouble();
+
+            employees.add(new Employee(id, name, salary));
+        }
+
+        System.out.println();
+        System.out.print("Enter the employee id that will have salary increase: ");
+        int idIncrease = input.nextInt();
+
+        Employee employeeIncrease = new Employee();
+
+        for(Employee emp : employees) {
+            if(emp.getId() == idIncrease) {
+                employeeIncrease = emp;
+                break;
+            }
+        }
+
+        if (employeeIncrease != null) {
+            System.out.print("Enter the percentage: ");
+            double percentage = input.nextDouble();
+            employeeIncrease.increaseSalary(percentage);
+        }
+
+
+        System.out.println("\nList of employees:");
+        for (Employee emp : employees) {
+            System.out.println(emp);
+        }
+
+        input.close();
+
+    }
 }
